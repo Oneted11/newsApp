5cd1b98b91c347badebeb94f485a479004976b81
Oneted11 <muriithited@gmail.com>
2018-10-05 12:47:53 +0300

    First commit, complete working thingie,updates forthcoming

---
+++ src/news.js
@@ -0,0 +1,7 @@
+const url =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=31789d30491e4d8884ff4c7e120c373b"
+
+export async function getNews() {
+    let result = await fetch(url).then(response => response.json());
+    return result.articles;
+}
